53716c4633bb9d7d2fa16c1d75ec2ec11d441ac6
Rahidul Islam <rahiseli@outlook.com>
2023-10-15 11:24:09 +0600

    navbar added

---
+++ frontend/src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+const Hello = () => {
+  return (
+    <>
+      <h2>Hello</h2>
+      <Button variant="primary">Hello</Button>
+    </>
+  );
+};
+
+export default Hello;
