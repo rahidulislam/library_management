9ad386b439b5cc2160dfdaf03bdd87346dd1366f
Rahidul Islam <rahiseli@outlook.com>
2024-08-25 14:46:29 +0600

    library branch remove from memberapplication

--- backend/membership/admin.py
+++ backend/membership/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Member, MemberSubscription, MemberApplication, SubscriptionPlan
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(MemberSubscription)
+admin.site.register(MemberApplication)
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'duration_months', 'price')
--- backend/membership/models.py
+++ backend/membership/models.py
@@ -7,6 +7,59 @@ from library.models import LibraryBranch
 # Create your models here.
 
 
+class SubscriptionPlan(models.Model):
+    class SubscriptionType(models.TextChoices):
+        MONTHLY = "MONTHLY", "Monthly"
+        YEARLY = "YEARLY", "Yearly"
+        LIFETIME = "LIFETIME", "Lifetime"
+
+    name = models.CharField(max_length=100)
+    subscription_type = models.CharField(
+        max_length=20, choices=SubscriptionType.choices
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration_months = models.PositiveIntegerField(
+        help_text="Duration of the plan in months. For lifetime, set it to a high number like 999."
+    )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(TimeStamp):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="member")
+    library_branch = models.ForeignKey(
+        LibraryBranch, on_delete=models.CASCADE, related_name="members"
+    )
+    membership_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class MemberSubscription(models.Model):
+    member = models.OneToOneField(
+        "Member", on_delete=models.CASCADE, related_name="subscription"
+    )
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.PROTECT)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.end_date:
+            self.end_date = self.start_date + timedelta(
+                days=30 * self.subscription_plan.duration_months
+            )
+        super().save(*args, **kwargs)
+
+    def is_active(self):
+        return self.end_date >= timezone.now()
+
+    def __str__(self):
+        return f"{self.member.user.email} - {self.subscription_plan.name}"
+
+
 class MemberApplication(TimeStamp):
     class Status(models.TextChoices):
         PENDING = "PENDING", "Pending"
@@ -21,9 +74,10 @@ class MemberApplication(TimeStamp):
     status = models.CharField(
         max_length=10, choices=Status.choices, default=Status.PENDING
     )
-    library_branch = models.ForeignKey(
-        LibraryBranch, on_delete=models.CASCADE, related_name="member_applications"
-    )
+    # library_branch = models.ForeignKey(
+    #     LibraryBranch, on_delete=models.CASCADE, related_name="member_applications"
+    # )
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, related_name="member_applications")
     reject_feedback = models.TextField(blank=True)
 
     class Meta:
@@ -32,48 +86,3 @@ class MemberApplication(TimeStamp):
 
     def __str__(self):
         return self.user.email
-
-class SubscriptionPlan(models.Model):
-    class SubscriptionType(models.TextChoices):
-        MONTHLY = 'MONTHLY', 'Monthly'
-        YEARLY = 'YEARLY', 'Yearly'
-        LIFETIME = 'LIFETIME', 'Lifetime'
-
-    name = models.CharField(max_length=100)
-    subscription_type = models.CharField(max_length=20, choices=SubscriptionType.choices)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration_months = models.PositiveIntegerField(help_text="Duration of the plan in months. For lifetime, set it to a high number like 999.")
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Member(TimeStamp):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="member")
-    library_branch = models.ForeignKey(
-        LibraryBranch, on_delete=models.CASCADE, related_name="members"
-    )
-    membership_date = models.DateField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return self.user.username
-
-
-class MemberSubscription(models.Model):
-    member = models.OneToOneField('Member', on_delete=models.CASCADE, related_name='subscription')
-    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.PROTECT)
-    start_date = models.DateField(default=timezone.now)
-    end_date = models.DateField()
-
-    def save(self, *args, **kwargs):
-        if not self.end_date:
-            self.end_date = self.start_date + timedelta(days=30 * self.subscription_plan.duration_months)
-        super().save(*args, **kwargs)
-
-    def is_active(self):
-        return self.end_date >= timezone.now()
-
-    def __str__(self):
-        return f"{self.member.user.email} - {self.subscription_plan.name}"
